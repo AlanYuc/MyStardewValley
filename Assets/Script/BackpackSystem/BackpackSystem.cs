7fcab998b818a9f144cebad1c4d424e57ff0968d
AlanYuc <alan.yuc@outlook.com>
2025-10-20 16:51:12 +0800

    创建背包相关的类

---
+++ Assets/Script/BackpackSystem/BackpackSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackpackSystem : MonoBehaviour
+{
+    /// <summary>
+    /// 背包系统的单例
+    /// </summary>
+    public static BackpackSystem Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
