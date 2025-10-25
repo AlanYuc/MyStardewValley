8d89430076e770a45639bdfe269d2dafc4ca68d0
AlanYuc <alan.yuc@outlook.com>
2025-10-25 16:58:42 +0800

    添加单例，获取金币模块

--- Assets/Script/TradeSystem/TradeSystem.cs
+++ Assets/Script/TradeSystem/TradeSystem.cs
@@ -2,8 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 交易系统
+/// </summary>
 public class TradeSystem : MonoBehaviour
 {
+    public static TradeSystem Instance;
+    /// <summary>
+    /// 金币模块
+    /// </summary>
+    public CoinModule coinModule;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        coinModule = GameObject.Find("CoinModule").GetComponent<CoinModule>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
