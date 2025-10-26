fa7992b589187270b601e01d1cc583f96233783e
AlanYuc <alan.yuc@outlook.com>
2025-10-26 16:52:19 +0800

    增加注释

--- Assets/Script/BackpackSystem/Backpack.cs
+++ Assets/Script/BackpackSystem/Backpack.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ±³°üµÄ±³°ü
+/// </summary>
 public class Backpack : InventoryContainer
 {
     // Start is called before the first frame update
--- Assets/Script/BackpackSystem/Toolbar.cs
+++ Assets/Script/BackpackSystem/Toolbar.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 工具栏的背包
+/// </summary>
 public class Toolbar : InventoryContainer
 {
     // Start is called before the first frame update
