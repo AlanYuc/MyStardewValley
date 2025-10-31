35a231f153b2c39bba0399b4849a3628b7ec91e7
AlanYuc <alan.yuc@outlook.com>
2025-10-31 17:44:34 +0800

    添加环境系统

---
+++ Assets/Script/EnvironmentSystem/EnvironmentSystem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 环境系统
+/// </summary>
+public class EnvironmentSystem : MonoBehaviour
+{
+    public static EnvironmentSystem Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
