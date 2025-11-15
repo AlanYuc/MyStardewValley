202ee95a04dc4c5313f1f22a680d290fb04c7971
AlanYuc <alan.yuc@outlook.com>
2025-11-15 15:37:56 +0800

    添加各类存档数据

--- Assets/Script/SaveSystem/SaveData.cs
+++ Assets/Script/SaveSystem/SaveData.cs
@@ -18,6 +18,7 @@ public class SaveData
 /// <summary>
 /// 供存档使用的玩家数据
 /// </summary>
+[Serializable]
 public class PlayerSaveData
 {
     /// <summary>
@@ -57,6 +58,7 @@ public class PlayerSaveData
 /// <summary>
 /// 供存档使用的生理数据
 /// </summary>
+[Serializable]
 public class PhysiologicalSaveData
 {
     /// <summary>
@@ -72,6 +74,7 @@ public class PhysiologicalSaveData
 /// <summary>
 /// 供存档使用的贸易数据
 /// </summary>
+[Serializable]
 public class TradeSaveData
 {
     /// <summary>
@@ -83,6 +86,7 @@ public class TradeSaveData
 /// <summary>
 /// 供存档使用的时间数据
 /// </summary>
+[Serializable]
 public class TimeSaveData
 {
     public int currentYear;
@@ -90,31 +94,108 @@ public class TimeSaveData
     public int currentDay;
     public int hour;
     public int minute;
+    /// <summary>
+    /// 计时器
+    /// </summary>
     public float gameTimer;
+    /// <summary>
+    /// 游戏内时间
+    /// </summary>
     public float gameTimeSecond;
+    /// <summary>
+    /// 真实时间
+    /// </summary>
     public float realTimeSecond;
 }
 
 /// <summary>
 /// 供存档使用的背包数据
 /// </summary>
+[Serializable]
 public class BackpackSaveData
 {
+    /// <summary>
+    /// 当前激活的格子，红框的位置
+    /// </summary>
     public int active_slot;
+    /// <summary>
+    /// 工具栏物品列表
+    /// </summary>
+    public List<ItemData> toolbar_item_list = new List<ItemData>();
+    /// <summary>
+    /// 背包物品列表
+    /// </summary>
+    public List<ItemData> backpack_item_list = new List<ItemData>();
+    /// <summary>
+    /// 丢在地上的物品列表
+    /// </summary>
+    public List<DiscardItemData> pickup_items_list = new List<DiscardItemData>();
+    /// <summary>
+    /// 储物箱物品列表
+    /// </summary>
+    public List<BoxData> box_data_list = new List<BoxData>();
 }
 
 /// <summary>
 /// 供存档使用的种植数据
 /// </summary>
+[Serializable]
 public class PlantSaveData
 {
-
+    /// <summary>
+    /// 二维网格，每个网格都有存储对应土壤的种植信息
+    /// </summary>
+    public GridCellData[,] gridData;
 }
 
 /// <summary>
 /// 供存档使用的环境数据
 /// </summary>
+[Serializable]
 public class EnvironmentSaveData
 {
+    /// <summary>
+    /// 场景内的环境物体
+    /// 保存的是物品id，以及该id的所有物品
+    /// </summary>
+    public Dictionary<int, List<EasyData>> stateless_objects = new Dictionary<int, List<EasyData>>();
+}
 
+/// <summary>
+/// 简单数据，目前只记录Transform
+/// </summary>
+[Serializable]
+public class EasyData
+{
+    public float x;
+    public float y;
+    public float z;
+
+    //rotate
+    //scale
+}
+
+/// <summary>
+/// 丢弃物品类
+/// </summary>
+[Serializable]
+public class DiscardItemData
+{
+    //丢弃物品的世界坐标
+    public float x;
+    public float y;
+    public float z;
+    public ItemData item_data = new ItemData();
+}
+
+/// <summary>
+/// 储物箱物品类
+/// </summary>
+[Serializable]
+public class BoxData
+{
+    public float x;
+    public float y;
+    public float z;
+    public List<ItemData> item_datas = new List<ItemData>();
 }
