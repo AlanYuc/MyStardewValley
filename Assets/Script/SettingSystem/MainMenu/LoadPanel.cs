4862f3d5640ea2a71f92124173ff0203a7e0fddb
AlanYuc <alan.yuc@outlook.com>
2025-11-15 15:38:15 +0800

    打开面板时加载数据

--- Assets/Script/SettingSystem/MainMenu/LoadPanel.cs
+++ Assets/Script/SettingSystem/MainMenu/LoadPanel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,6 +15,10 @@ public class LoadPanel : BasePanel
     /// 存放所有存档的区域
     /// </summary>
     public Transform _loadContent;
+    /// <summary>
+    /// 存档列表显示的prefab
+    /// </summary>
+    public GameObject loadItem;
 
     public override void Awake()
     {
@@ -21,6 +26,7 @@ public class LoadPanel : BasePanel
 
         _backButton     = transform.Find("Back_Btn").GetComponent<Button>();
         _loadContent    = transform.Find("LoadBackground_Bg/Scroll View/Viewport/Load_Content").GetComponent<Transform>();
+        loadItem        = Resources.Load<GameObject>("Prefab/LoadItem");
     }
 
     // Start is called before the first frame update
@@ -44,7 +50,37 @@ public class LoadPanel : BasePanel
     {
         base.OpenPanel();
 
-        //读取所有存档，并展示
+        //读取所有存档
+        List<SaveData> saveDataList = SaveSystem.Instance.GetSaveDataList();
+
+        int i = 0;//索引
+
+        //展示所有存档
+        foreach (SaveData saveData in saveDataList)
+        {
+            //生成一个存档对象
+            GameObject go = Instantiate(loadItem, _loadContent);
+
+            //获取引用
+            TMP_Text playerName = go.transform.Find("PlayerName").GetComponent<TMP_Text>();
+            TMP_Text farmName = go.transform.Find("FarmName").GetComponent<TMP_Text>();
+            TMP_Text gameTime = go.transform.Find("GameTime").GetComponent<TMP_Text>();
+            TMP_Text realTime = go.transform.Find("RealTime").GetComponent<TMP_Text>();
+            TMP_Text money = go.transform.Find("Money").GetComponent<TMP_Text>();
+            TMP_Text number = go.transform.Find("Number").GetComponent<TMP_Text>();
+
+            //游戏内时间
+            //真实时间
+
+            //更新信息
+            playerName.text = saveData.playerSaveData.player_name;
+            farmName.text = saveData.playerSaveData.farm_name;
+            money.text = saveData.tradeSaveData.coins.ToString();
+            number.text = i.ToString();
+
+            i++;
+        }
+
         //点击对应存档加载游戏
     }
 
